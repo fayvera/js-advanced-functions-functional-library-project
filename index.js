6a2a090203d9730877600ab8e69a48a064b11433
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-27 14:35:26 -0400

    commit tree c627ae4976118299e666ec82e435ff913b03dcd2

--- index.js
+++ index.js
@@ -4,17 +4,41 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      const newColl = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      for (let i = 0; i < newColl.length; i++)
+        callback(newColl[i])
+      return collection
     },
 
-    map: function() {
 
+    map: function(collection, callback) {
+
+      if (!(collection instanceof Array))
+      collection = Object.values(collection)
+
+      const newArr = []
+      for (let i = 0; i < collection.length; i++){
+        newArr.push(callback(collection[i]))
+      }
+      return newArr
     },
 
-    reduce: function() {
+    reduce: function(c = [], callback = () => {}, acc) {
 
+      let collection = [...c]
+      if (!acc){
+        let acc = collection[0]
+        debugger 
+        collection = collection.slice(1)
+      }
+      for (let i = 0; i < collection.length; i++){
+        acc = callback(acc, collection[i], collection)
+      }
+      return acc
     },
+
+
 
     functions: function() {
 
@@ -25,3 +49,7 @@ const fi = (function() {
 })()
 
 fi.libraryMethod()
+
+
+
+// fi.reduce([1, 2, 3, 4], function(acc, val, collection) { return acc + val; })
